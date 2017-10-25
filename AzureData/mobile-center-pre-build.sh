c886e88b7d1c3c62b053b6b0df5be1a2121de5d5
Colby Williams <colbylwilliams@gmail.com>
2017-10-25 13:19:47 -0400

    test autoset env vars

--- AzureData/mobile-center-pre-build.sh
+++ AzureData/mobile-center-pre-build.sh
@@ -1,33 +1,36 @@
 #!/usr/bin/env bash
 
-accountName="$AZURE_COSMOS_DB_ACCOUNT_NAME"
-accountKey="$AZURE_COSMOS_DB_ACCOUNT_KEY"
+AZURE_COSMOS_DB_ACCOUNT="$AZURE_COSMOS_DB_ACCOUNT_NAME"
+AZURE_COSMOS_DB_KEY="$AZURE_COSMOS_DB_ACCOUNT_KEY"
 
-echo Account Name: "$accountName"
-echo Account Key: "$accountKey"
+# accountName="$AZURE_COSMOS_DB_ACCOUNT_NAME"
+# accountKey="$AZURE_COSMOS_DB_ACCOUNT_KEY"
 
-
-echo Source Directory: "$MOBILECENTER_SOURCE_DIRECTORY"
-
-
-frmkInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/AzureData/Info.plist"
-appsInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/AzureDataApp/Info.plist"
-testInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/AzureDataTests/Info.plist"
+# echo Account Name: "$accountName"
+# echo Account Key: "$accountKey"
 
 
-echo Framework Info.plist Path: "$frmkInfoPlist"
-echo App Info.plist Path: "$appsInfoPlist"
-echo Test Info.plist Path: "$testInfoPlist"
+# echo Source Directory: "$MOBILECENTER_SOURCE_DIRECTORY"
 
 
-plutil -replace ADDatabaseAccountName -string "$accountName" "$frmkInfoPlist"
+# frmkInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/AzureData/Info.plist"
+# appsInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/AzureDataApp/Info.plist"
+# testInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/AzureDataTests/Info.plist"
 
-plutil -replace ADDatabaseAccountName -string "$accountName" "$appsInfoPlist"
 
-plutil -replace ADDatabaseAccountName -string "$accountName" "$testInfoPlist"
+# echo Framework Info.plist Path: "$frmkInfoPlist"
+# echo App Info.plist Path: "$appsInfoPlist"
+# echo Test Info.plist Path: "$testInfoPlist"
 
-plutil -replace ADDatabaseAccountKey -string "$accountKey" "$frmkInfoPlist"
 
-plutil -replace ADDatabaseAccountKey -string "$accountKey" "$appsInfoPlist"
+# plutil -replace ADDatabaseAccountName -string "$accountName" "$frmkInfoPlist"
 
-plutil -replace ADDatabaseAccountKey -string "$accountKey" "$testInfoPlist"
+# plutil -replace ADDatabaseAccountName -string "$accountName" "$appsInfoPlist"
+
+# plutil -replace ADDatabaseAccountName -string "$accountName" "$testInfoPlist"
+
+# plutil -replace ADDatabaseAccountKey -string "$accountKey" "$frmkInfoPlist"
+
+# plutil -replace ADDatabaseAccountKey -string "$accountKey" "$appsInfoPlist"
+
+# plutil -replace ADDatabaseAccountKey -string "$accountKey" "$testInfoPlist"

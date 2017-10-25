9f63b3fcaced457c16153dd64bd4928a6e8d04e4
Colby Williams <colbylwilliams@gmail.com>
2017-10-25 12:51:41 -0400

    set info.plist vars via bash

--- AzureData/mobile-center-pre-build.sh
+++ AzureData/mobile-center-pre-build.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
-AZURE_COSMOS_DB_ACCOUNT_NAME="$AZURE_COSMOS_DB_ACCOUNT_NAME"
+accountName="$AZURE_COSMOS_DB_ACCOUNT_NAME"
+accountKey="$AZURE_COSMOS_DB_ACCOUNT_KEY"
 
-AZURE_COSMOS_DB_ACCOUNT_KEY="$AZURE_COSMOS_DB_ACCOUNT_KEY"
+echo Account Name: "$accountName"
+echo Account Key: "$accountKey"
 
-echo "$AZURE_COSMOS_DB_ACCOUNT_NAME"
 
-echo "$AZURE_COSMOS_DB_ACCOUNT_KEY"
+echo Source Directory: "$MOBILECENTER_SOURCE_DIRECTORY"
+
+
+fmkInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/AzureData/Info.plist"
+appInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/AzureDataApp/Info.plist"
+
+
+echo Framework Info.plist Path: "$fmkInfoPlist"
+echo App Info.plist Path: "$appInfoPlist"
+
+
+plutil -replace ADDatabaseAccountName -string "$accountName" "$fmkInfoPlist"
+
+plutil -replace ADDatabaseAccountName -string "$accountName" "$appInfoPlist"
+
+plutil -replace ADDatabaseAccountKey -string "$accountKey" "$fmkInfoPlist"
+
+plutil -replace ADDatabaseAccountKey -string "$accountKey" "$appInfoPlist"

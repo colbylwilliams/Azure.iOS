23865675c1ce712cfd0a12cdb5d150a4973b0771
Colby Williams <colbylwilliams@gmail.com>
2017-11-08 14:32:07 -0500

    use carthage instead of direct link in ios sample

--- AzureData/mobile-center-pre-build.sh
+++ AzureData/mobile-center-pre-build.sh
@@ -1,24 +1,10 @@
 #!/usr/bin/env bash
 
-accountName="$AZURE_COSMOS_DB_ACCOUNT_NAME"
-accountKey="$AZURE_COSMOS_DB_ACCOUNT_KEY"
-
-# echo Account Name: "$accountName"
-# echo Account Key: "$accountKey"
-
-
-# echo Source Directory: "$MOBILECENTER_SOURCE_DIRECTORY"
-
 # frmkInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/Source/Info.plist"
 testInfoPlist="$MOBILECENTER_SOURCE_DIRECTORY/AzureData/Tests/Info.plist"
-
-
-# echo Framework Info.plist Path: "$frmkInfoPlist"
-# echo Test Info.plist Path: "$testInfoPlist"
-
 
 # plutil -replace ADDatabaseAccountName -string "$accountName" "$frmkInfoPlist"
 # plutil -replace ADDatabaseAccountKey -string "$accountKey" "$frmkInfoPlist"
 
-plutil -replace ADDatabaseAccountName -string "$accountName" "$testInfoPlist"
-plutil -replace ADDatabaseAccountKey -string "$accountKey" "$testInfoPlist"
+plutil -replace ADDatabaseAccountName -string "$AZURE_COSMOS_DB_ACCOUNT_NAME" "$testInfoPlist"
+plutil -replace ADDatabaseAccountKey -string "$AZURE_COSMOS_DB_ACCOUNT_KEY" "$testInfoPlist"
